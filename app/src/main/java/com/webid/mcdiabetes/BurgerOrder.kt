e8cc07942ab1fd0644e1a2b34eac5d84b70f49de
sarinacm@gmail.com <sarinacm@gmail.com>
2023-05-31 11:15:53 +0200

    initial commit -firebase auth reg

---
+++ app/src/main/java/com/webid/mcdiabetes/BurgerOrder.kt
@@ -0,0 +1,11 @@
+package com.webid.mcdiabetes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BurgerOrder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_burger_order)
+    }
+}
---
+++ app/src/main/java/com/webid/mcdiabetes/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.webid.mcdiabetes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val et_username = findViewById<EditText>(R.id.et_username)
+        val et_password = findViewById<EditText>(R.id.et_password)
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        val btn_register = findViewById<Button>(R.id.btn_regsiter)
+
+       lateinit var auth: FirebaseAuth
+        auth = Firebase.auth
+
+        btn_register.setOnClickListener()
+        {
+            val username = et_username.text.toString()
+            val password= et_password.text.toString()
+            auth.createUserWithEmailAndPassword(username,password).addOnCompleteListener(this)
+            {
+                task->
+
+                if(task.isSuccessful)
+                {
+
+                    val intent = Intent(this,BurgerOrder::class.java)
+                    startActivity(intent)
+
+                }
+                else
+                {
+                   Toast.makeText(this, "Please ensure that you have entered your details " +
+                           "correctly", Toast.LENGTH_LONG).show()
+                }
+
+            }
+                .addOnFailureListener(this)
+                {
+                    task->
+
+                    Toast.makeText(this,task.message.toString(),Toast.LENGTH_LONG).show()
+                }
+
+        }
+
+
+    }
+}
